b842b0e01502be37bce1b9f853d1aff53ba2df37
Asad-Kahloon <asadkahloon77@gmail.com>
2024-06-16 00:32:30 +0500

    currency converter completed

--- react_learning/src/App.jsx
+++ react_learning/src/App.jsx
@@ -2,7 +2,10 @@
 // import Bg_Changer from "./Project01/Bg_Changer";
 
 // Project 02
-import Pass_Generator from "./Project02/Pass_Generator";
+// import Pass_Generator from "./Project02/Pass_Generator";
+
+// Project 03
+import Run from "./Project03/Run";
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
       {/* <Bg_Changer /> */}
 
       {/* Project 02 */}
-      <Pass_Generator />
+      {/* <Pass_Generator /> */}
+
+      {/* Project 03 */}
+      <Run />
     </>
   );
 }
