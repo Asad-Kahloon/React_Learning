36bdef0b73e721d78c1c9c3bd14c0b2443148bbd
Asad-Kahloon <asadkahloon77@gmail.com>
2024-06-17 00:15:56 +0500

    rout

--- react_learning/src/Project04/Rout.jsx
+++ react_learning/src/Project04/Rout.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
 import About from "./About/About";
+import Contact from "./Contact/Contact";
 
 function Rout() {
   return (
@@ -8,6 +9,7 @@ function Rout() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
